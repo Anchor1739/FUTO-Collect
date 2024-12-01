25c869e904e6e1624929f7c7d95a3f6e304950e0
Anchor1739 <anchorgodsown@gmail.com>
2024-12-01 14:08:05 +0100

    Update generate_receipt.php

--- generate_receipt.php
+++ generate_receipt.php
@@ -1,37 +1,14 @@
 <?php
 header('Content-Type: application/json');
 
-// Allow only POST requests 
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    http_response_code(405); // Method Not Allowed
-    echo json_encode(["error" => "Only POST requests are allowed"]);
-    exit;
-}
 
-// Get input from POST request
-$inputData = json_decode(file_get_contents('php://input'), true);
 
-// Validate inputs
-if (!isset($inputData['cardNumber']) || !isset($inputData['plateNumber'])) {
-    http_response_code(400); // Bad Request
-    echo json_encode(["error" => "Missing required parameters: cardNumber and plateNumber"]);
-    exit;
-}
-
-$cardNumber = $inputData['cardNumber'];
-$plateNumber = $inputData['plateNumber'];
-
-// length checker
-function validateLength($input, $length) {
-    return strlen($input) === $length;
-}
 //return if user send empty inputs
 if (empty($cardNumber) || empty($plateNumber)) {
     http_response_code(400);
     echo json_encode(["error" => "Invalid input values: cardNumber and plateNumber cannot be empty"]);
     exit;
 }
-
 // Verify length of inputs cardNumber must be 16 digits and  plateNumber 7 characters)
 if (!validateLength($cardNumber, 16) || !validateLength($plateNumber, 7)) {
     http_response_code(400);
